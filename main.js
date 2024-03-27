2ef27013f87622ef8c1da31d2126051a565ba830
myrellalinhares <107002111+myrellalinhares@users.noreply.github.com>
2024-03-27 11:52:31 -0300

    27/03

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const botoes = document.querySelectorAll(".botao");//variável que armazena todos os itens com a classe botão
- 
+ const botoes=document.querySelector(".aba-conteudo")//variável que armazena todos os itens com a classe aba conteudo
 
 for(let i=0;i <botoes.length;i++){//loop que executa enquanto i<quantidade de botoes
     botoes[i].onclick = function(){//cria uma função
         
         for(let j=0;j<botoes.length;j++){ //loop que executa j<quantidade de botoes
             botoes[j].classList.remove("ativo");//remove a palavra ativo da classe.
+            textos[j].classList.remove("ativo");//remove a palavra ativo da classe.
         } 
         
         botoes[i].classList.add("ativo");//adiciona a palavra ativo da classe.
+        textos[i].classList.add("ativo");//adiciona a palavra ativo da classe.
     }
     console.log();
 }
